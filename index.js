420d3dcb77c0e0d58eddf31fe6bccf5b6ef8e98f
Kasey Arnold <kaseyarnold15@gmail.com>
2020-11-09 15:36:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+document.body.appendChild(element)
+
+let ul = document.createElement('ul')
+ 
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li')
+  li.innerHTML = (i + 1).toString()
+  ul.appendChild(li)
+}
+ 
+element.appendChild(ul)
+
+let element = document.querySelector("p#greeting");
+element.innerHTML = 'Hello, DOM!'
