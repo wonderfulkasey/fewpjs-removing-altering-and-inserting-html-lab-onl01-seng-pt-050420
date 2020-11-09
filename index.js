d38722b7de81358eb91aafc2d098ccaf75624834
Kasey Arnold <kaseyarnold15@gmail.com>
2020-11-09 15:39:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+main.remove()
+
+
+let newHeader = document.createElement('h1');
+
+document.body.appendChild(newHeader);
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "YOUR-NAME is the champion"; 
